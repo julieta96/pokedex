fe788c9310fa7ad0c132715143ebc4e2d54a8fe7
julieta96 <julietaextra14@gmail.com>
2021-05-14 21:11:05 -0300

    se modifico la tabla

--- index.php
+++ index.php
@@ -47,139 +47,73 @@ if(isset($_SESSION['usuario'])){
   <table class="table container">
   <thead>
     <tr>
-      <th scope="col">Imagen</th>
-      <th scope="col">Tipo</th>
-      <th scope="col">Numero</th>
+      <th scope="col">Nro</th>
       <th scope="col">Nombre</th>
+      <th scope="col">Altura</th>
+      <th scope="col">Peso</th>
+      <th scope="col">Habilidad</th>
+      <th scope="col">Tipo</th>
+      <th scope="col">Descripcion</th>
+      <th scope="col">Imagen</th>
     </tr>
   </thead>
   <tbody>
+  	   <?php 
+
+  	   $sql="SELECT * FROM Pokemons";
+  	   $result = $con->query($sql);
+  	   $pokemons = Array();
+
+  	   foreach($pokemons as $pokemon){}
+
+  	   	if (mysqli_num_rows($result) > 0) {
+   
+		    while($fila = mysqli_fetch_assoc($result)) {
+		        $c = Array();
+		        $pokemon['id'] =  $fila["id_manual"];
+		        $pokemon['nombre'] =  $fila["nombre"];
+		        $pokemon['altura'] =  $fila["altura"];
+		        $pokemon['peso'] =  $fila["peso"];
+		        $pokemon['habilidad'] =  $fila["habilidad"];
+		        $pokemon['tipo'] =  $fila["tipo"];
+		        if($fila["tipo_dos"]!=null){
+
+		        $pokemon['tipo_dos'] =  $fila["tipo_dos"];
+
+		        }
+		        $pokemon['descripcion'] =  $fila["descripcion"];
+		        $pokemon['imagen'] =  $fila["imagen"];
+		        $pokemons[] = $pokemon;
+    		}
+
+
+  		}
+
+  		foreach ( $pokemons as $pokemon){
+                    echo   "<tr>
+                                <td>" . $pokemon['id'] . "</td>
+                                <td>" . $pokemon['nombre'] . "</td>
+                                <td>" . $pokemon['altura'] . "</td>
+                                <td>" . $pokemon['peso'] . "</td>
+                                <td>" . $pokemon['habilidad'] . "</td>
+                                <td><img src=" . $pokemon['tipo'] . "width=70 height=70>";
+
+	                            if($pokemon["tipo_dos"]!=null){
+
+			        			  echo" <img src=".$pokemon['tipo_dos']. "width=70 height=70></td>";
+
+			        			}else{
+			        				 echo"</td>";
+			        			} 
+
+                              echo "<td>" . $pokemon['descripcion'] . "</td>
+                                   <td> <img src=" . $pokemon['imagen'] . " width=75 height=75 ></td>
+
+                             </tr>";
+                }?>
+
+  	
     <tr>
-      <th scope="row">
-      	<?php 
-            
-		 
-			$sql = "select p.imagen 
-			 		from pokemons p
-			 		where p.nombre like '%pikachu%'";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-					echo"<img src=".$fila["imagen"]." style='width: 5rem; height: 80px'>";
-			 }
-				
-
-      	?>
-      </th>
-      <td>
-      	<?php 
-   
-			$sql = "select t.descripcion 
-			 		from tipo t inner join 
-			 		pokemons p on p.tipo=t.id
-			 		where p.nombre like '%pikachu%'";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-					 echo"<img src=".$fila["descripcion"]." style='width: 5rem; height: 80px'>";
-			 }
-				
-
-      	?>
-      </td>
-      <td>
-      		<?php 
-   
-			$sql = "select p.id 
-			 		from  pokemons p 
-			 		where p.nombre like '%pikachu%'";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-			 	    $id=$fila["id"];
-					echo $id;
-			 }
-				
-
-      	?>
-      </td>
-      <td>
-      		<?php 
-   
-			 $sql = "select p.nombre 
-			 		from  pokemons p 
-			 		where p.id=1";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-			 	    $nom=$fila["nombre"];
-					echo $nom;
-			 }
-				
-      	?>
-      </td>
-    </tr>
-    <tr>
-      <th scope="row">
-      	 	<?php 
-            
-		 
-			$sql = "select p.imagen 
-			 		from pokemons p
-			 		where p.nombre like '%squirtle%'";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-					echo"<img src=".$fila["imagen"]." style='width: 5rem; height: 80px'>";
-			 }
-
-			 ?>
-      </th>
-      <td><?php 
-   
-			$sql = "select t.descripcion 
-			 		from tipo t inner join 
-			 		pokemons p on p.tipo=t.id
-			 		where p.nombre like '%squirtle%'";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-					 echo"<img src=".$fila["descripcion"]." style='width: 5rem; height: 80px'>";
-			 }
-				
-
-      	?></td>
-      <td>
-      	<?php 
-   
-			$sql = "select p.id 
-			 		from  pokemons p 
-			 		where p.nombre like '%squirtle%'";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-			 	    $id=$fila["id"];
-					echo $id;
-			 }
-				
-
-      	?>
-      </td>
-      <td>
-      		<?php 
-   
-			 $sql = "select p.nombre 
-			 		from  pokemons p 
-			 		where p.id=2";
-			$result = $con->query($sql);
-			$resultArray=$result->fetch_all(MYSQLI_ASSOC);
-			 foreach($resultArray as $fila){
-			 	    $nom=$fila["nombre"];
-					echo $nom;
-			 }
-				
-      	?>
-      </td>
     </tr>
   </tbody>
 </table>
--- pagina-logueado.php
+++ pagina-logueado.php
@@ -10,157 +10,87 @@ include('conexion.php');
 			<input type="text"  class="form-control" name="buscar" placeholder="Ingrese nombre, tipo o numero de pokemon"><button type="submit" class="btn btn-primary">Quien es este pokemon?</button>
 		</form>
 	 </section>
-
-  <table class="table container">
+   <table class="table container">
   <thead>
     <tr>
-      <th scope="col">Imagen</th>
-      <th scope="col">Tipo</th>
-      <th scope="col">Numero</th>
+      <th scope="col">Nro</th>
       <th scope="col">Nombre</th>
+      <th scope="col">Altura</th>
+      <th scope="col">Peso</th>
+      <th scope="col">Habilidad</th>
+      <th scope="col">Tipo</th>
+      <th scope="col">Descripcion</th>
+      <th scope="col">Imagen</th>
       <th scope="col">Acciones</th>
     </tr>
   </thead>
   <tbody>
+       <?php 
+
+       $sql="SELECT * FROM Pokemons";
+       $result = $con->query($sql);
+       $pokemons = Array();
+
+       foreach($pokemons as $pokemon){}
+
+        if (mysqli_num_rows($result) > 0) {
+   
+        while($fila = mysqli_fetch_assoc($result)) {
+            $c = Array();
+            $pokemon['id'] =  $fila["id_manual"];
+            $pokemon['nombre'] =  $fila["nombre"];
+            $pokemon['altura'] =  $fila["altura"];
+            $pokemon['peso'] =  $fila["peso"];
+            $pokemon['habilidad'] =  $fila["habilidad"];
+            $pokemon['tipo'] =  $fila["tipo"];
+            if($fila["tipo_dos"]!=null){
+
+            $pokemon['tipo_dos'] =  $fila["tipo_dos"];
+
+            }
+            $pokemon['descripcion'] =  $fila["descripcion"];
+            $pokemon['imagen'] =  $fila["imagen"];
+            $pokemons[] = $pokemon;
+        }
+
+
+      }
+
+      foreach ( $pokemons as $pokemon){
+                    echo   "<tr>
+                                <td>" . $pokemon['id'] . "</td>
+                                <td>" . $pokemon['nombre'] . "</td>
+                                <td>" . $pokemon['altura'] . "</td>
+                                <td>" . $pokemon['peso'] . "</td>
+                                <td>" . $pokemon['habilidad'] . "</td>
+                                <td><img src=" . $pokemon['tipo'] . "width=70 height=70>";
+
+                              if($pokemon["tipo_dos"]!=null){
+
+                      echo" <img src=".$pokemon['tipo_dos']. "width=70 height=70></td>";
+
+                    }else{
+                       echo"</td>";
+                    } 
+
+                              echo "<td>" . $pokemon['descripcion'] . "</td>
+                                   <td> <img src=" . $pokemon['imagen'] . " width=75 height=75 ></td>
+                                   <td>
+                                   <button type='button' class='btn btn-warning ms-auto'>MODIFICACION</button>
+                                   <br>
+                                   <button type='button' class='btn btn-danger ms-auto'>BAJA</button>
+                                   </td>
+
+                             </tr>";
+                }?>
+
+    
     <tr>
-      <th scope="row">
-        <?php 
-            
-     
-      $sql = "select p.imagen 
-          from pokemons p
-          where p.nombre like '%pikachu%'";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-         echo"<a href='http://localhost:8080/tp-pokedex/pokemons-detalle.php?imagen=".$fila["imagen"]."'>"."<img src=".$fila["imagen"]." style='width: 5rem; height: 80px'></a>";
-       }
-        
-
-        ?>
-      </th>
-      <td>
-        <?php 
-   
-      $sql = "select t.descripcion 
-          from tipo t inner join 
-          pokemons p on p.tipo=t.id
-          where p.nombre like '%pikachu%'";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-           echo"<img src=".$fila["descripcion"]." style='width: 5rem; height: 80px'>";
-       }
-        
-
-        ?>
-      </td>
-      <td>
-          <?php 
-   
-      $sql = "select p.id 
-          from  pokemons p 
-          where p.nombre like '%pikachu%'";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-            $id=$fila["id"];
-          echo $id;
-       }
-        
-
-        ?>
-      </td>
-      <td>
-          <?php 
-   
-       $sql = "select p.nombre 
-          from  pokemons p 
-          where p.id=1";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-            $nom=$fila["nombre"];
-          echo $nom;
-       }
-        
-        ?>
-      </td>
-      <td>
-         <button type="button" class="btn btn-warning mx-auto">MODIFICACION</button>
-         <button type="button" class="btn btn-danger mx-auto">BAJA</button></td>
-      </td>
     </tr>
-    <tr>
-      <th scope="row">
-          <?php 
-            
-     
-      $sql = "select p.imagen 
-          from pokemons p
-          where p.nombre like '%squirtle%'";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-         echo"<a href='http://localhost:8080/tp-pokedex/pokemons-detalle.php?imagen=".$fila["imagen"]."'>"."<img src=".$fila["imagen"]." style='width: 5rem; height: 80px'></a>";
-       }
-
-       ?>
-      </th>
-      <td><?php 
-   
-      $sql = "select t.descripcion 
-          from tipo t inner join 
-          pokemons p on p.tipo=t.id
-          where p.nombre like '%squirtle%'";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-           echo"<img src=".$fila["descripcion"]." style='width: 5rem; height: 80px'>";
-       }
-        
-
-        ?></td>
-      <td>
-        <?php 
-   
-      $sql = "select p.id 
-          from  pokemons p 
-          where p.nombre like '%squirtle%'";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-            $id=$fila["id"];
-          echo $id;
-       }
-        
-
-        ?>
-      </td>
-      <td>
-          <?php 
-   
-       $sql = "select p.nombre 
-          from  pokemons p 
-          where p.id=2";
-      $result = $con->query($sql);
-      $resultArray=$result->fetch_all(MYSQLI_ASSOC);
-       foreach($resultArray as $fila){
-            $nom=$fila["nombre"];
-          echo $nom;
-       }
-        
-        ?>
-      </td>
-        <td>
-         <button type="button" class="btn btn-warning mx-auto">MODIFICACION</button>
-         <button type="button" class="btn btn-danger mx-auto">BAJA</button></td>
-      </td>
-    </tr>
-
   </tbody>
 </table>
-<button class="btn btn-primary form-control btn-block container mx-5">NUEVO POKEMON</button>
+
+<button class="btn btn-primary form-control btn-block container mx-5 mb-3">NUEVO POKEMON</button>
 
 	<script type="text/javascript" src="recursos/js/bootstrap.min.js"></script>
 
