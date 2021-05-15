9c9be511735208e91f8c0bc8a68c817c751901d5
julieta96 <julietaextra14@gmail.com>
2021-05-14 21:05:45 -0300

    se modifico los datos de la conexion

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
   <?php  
-    $usuario="root";
-	$password="291121okayj";
+    $usuario="admin";
+	$password="12345";
 	$servidor="localhost";
 	$nombrebasedatos="pokedex";
 	$con= new mysqli($servidor,$usuario,$password,$nombrebasedatos)or die("error de conexion".mysql_error());
