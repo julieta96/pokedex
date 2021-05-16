bda14ca795fce380a7960e78957f57d14f6d9914
julieta96 <julietaextra14@gmail.com>
2021-05-15 23:11:25 -0300

    action + method + if que valida boton del form

--- pagina-logueado.php
+++ pagina-logueado.php
@@ -2,12 +2,13 @@
 session_start();
 include('header-con-sesion.php');
 include('conexion.php');
+include('busqueda-pokemon.php');
 ?>
 
 	<section class="d-flex flex-col mb-3 col">
 
-		<form class="form-group d-flex flex-col col">
-			<input type="text"  class="form-control" name="buscar" placeholder="Ingrese nombre, tipo o numero de pokemon"><button type="submit" class="btn btn-primary">Quien es este pokemon?</button>
+		<form class="form-group d-flex flex-col col" method="get" action="pagina-logueado.php">
+			<input type="text"  class="form-control" name="buscar" placeholder="Ingrese nombre, tipo o numero de pokemon"><button type="submit" class="btn btn-primary" name="boton">Quien es este pokemon?</button>
 		</form>
 	 </section>
    <table class="table container">
@@ -26,69 +27,26 @@ include('conexion.php');
   </thead>
   <tbody>
        <?php
+      if(isset($_GET['boton'])){
 
-       $sql="SELECT * FROM Pokemons";
-       $result = $con->query($sql);
-       $pokemons = Array();
+        include('obtener-pokemon.php');
 
-       foreach($pokemons as $pokemon){}
+      }else{
 
-        if (mysqli_num_rows($result) > 0) {
-   
-        while($fila = mysqli_fetch_assoc($result)) {
-            $c = Array();
-            $pokemon['id'] =  $fila["id_manual"];
-            $pokemon['nombre'] =  $fila["nombre"];
-            $pokemon['altura'] =  $fila["altura"];
-            $pokemon['peso'] =  $fila["peso"];
-            $pokemon['habilidad'] =  $fila["habilidad"];
-            $pokemon['tipo'] =  $fila["tipo"];
-            if($fila["tipo_dos"]!=null){
-
-            $pokemon['tipo_dos'] =  $fila["tipo_dos"];
-
-            }
-            $pokemon['descripcion'] =  $fila["descripcion"];
-            $pokemon['imagen'] =  $fila["imagen"];
-            $pokemons[] = $pokemon;
-        }
-
+       include('busqueda-pokemon.php');
+       include('tabla-pokemon.php');
 
       }
-
-      foreach ( $pokemons as $pokemon){
-                    echo   "<tr>
-                                <td>" . $pokemon['id'] . "</td>
-                                <td>" . $pokemon['nombre'] . "</td>
-                                <td>" . $pokemon['altura'] . "</td>
-                                <td>" . $pokemon['peso'] . "</td>
-                                <td>" . $pokemon['habilidad'] . "</td>
-                                <td><img src='".$pokemon['tipo']."' width='50' height='50'>";
-
-                              if($pokemon["tipo_dos"]!=null){
-
-                      echo" <img src='".$pokemon['tipo_dos']. "' width='50' height='50'></td>";
-
-                    }else{
-                       echo"</td>";
-                    } 
-
-                              echo "<td>" . $pokemon['descripcion'] . "</td>
-                                   <td> <img src=" . $pokemon['imagen'] . " width=75 height=75 ></td>
-                                   <td>
-                                   <button type='button' class='btn btn-warning ms-auto'>MODIFICACION</button>
-                                   <br>
-                                   <button type='button' class='btn btn-danger ms-auto'>BAJA</button>
-                                   </td>
-
-                             </tr>";
-                }?>
+                  
+    ?>
 
     
     <tr>
     </tr>
   </tbody>
 </table>
+
+
 
 <button class="btn btn-primary form-control btn-block container mx-5 mb-3">NUEVO POKEMON</button>
 
