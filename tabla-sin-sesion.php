8c7b8f252c0b05a1ceadb074c4e698fd5c31ce68
Tabatha <49240387+Tabathapm@users.noreply.github.com>
2021-05-28 20:36:49 -0300

    Algo de responsive

--- tabla-sin-sesion.php
+++ tabla-sin-sesion.php
@@ -1,27 +1,27 @@
 <?php
 
 foreach ( $pokemons as $pokemon){
-                    echo   "<tr>
-                                <td>" . $pokemon['id'] . "</td>
-                                <td>" . $pokemon['nombre'] . "</td>
-                                <td>" . $pokemon['altura'] . "</td>
-                                <td>" . $pokemon['peso'] . "</td>
-                                <td>" . $pokemon['habilidad'] . "</td>
-                                <td><img src='".$pokemon['tipo']."' width='50' height='50'>";
+    echo   "<tr>
+                                <td class='text-center'>" . $pokemon['id'] . "</td>
+                                <td class='text-center'>" . $pokemon['nombre'] . "</td>
+                                <td class='text-center'>" . $pokemon['altura'] . "</td>
+                                <td class='text-center'>" . $pokemon['peso'] . "</td>
+                                <td class='text-center'>" . $pokemon['habilidad'] . "</td>
+                                <td class='text-center'><img src='".$pokemon['tipo']."' width='50' height='50'>";
 
-                              if(isset($pokemon["tipo_dos"])){
+    if(isset($pokemon["tipo_dos"])){
 
-                      echo" <img src='".$pokemon['tipo_dos']. "' width='50' height='50'></td>";
+        echo" <img src='".$pokemon['tipo_dos']. "' width='50' height='50'></td>";
 
-                    }else{
-                       echo"</td>";
-                    } 
+    }else{
+        echo"</td>";
+    }
 
-                              echo "<td>" . $pokemon['descripcion'] . "</td>
+    echo "<td>" . $pokemon['descripcion'] . "</td>
                                    <td> <img src=" . $pokemon['imagen'] . " width=75 height=75 ></td>
                                    
                              </tr>";
-                }
+}
 
 
 
