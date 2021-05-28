f18941d2b4e02500576fbdc55bcf8be5c7bd917d
Tabatha <49240387+Tabathapm@users.noreply.github.com>
2021-05-28 20:32:34 -0300

    Similar a cargar un pokemon - pero no funciona

--- modificar.php
+++ modificar.php
@@ -1,28 +1,68 @@
 <?php
 
+if (isset($_POST['modificar'])) {
 
-$id = isset( $_POST["id"])?$_POST["id"] : "";
-$nombre = isset( $_POST["nombre"])?$_POST["nombre"] : "";
-$altura = isset( $_POST["altura"])?$_POST["altura"] : "";
-$peso = isset( $_POST["peso"])?$_POST["peso"] : "";
-$habilidad = isset( $_POST["habilidad"])?$_POST["habilidad"] : "";
-$tipo = isset( $_POST["tipo"])?$_POST["tipo"] : "";
-$tipo_dos = isset( $_POST["tipo_dos"])?$_POST["tipo_dos"] : "";
-$imagen = isset( $_POST["imagen"])?$_POST["imagen"] : "";
-$descripcion = isset( $_POST["descripcion"])?$_POST["descripcion"] : "";
+    $altura = isset( $_POST["altura"])?$_POST["altura"] : "";
+    $peso = isset( $_POST["peso"])?$_POST["peso"] : "";
+    $habilidad = isset( $_POST["habilidad"])?$_POST["habilidad"] : "";
+    $descrip = isset( $_POST["descripcion"])?$_POST["descripcion"] : "";
 
-include_once("conexion.php");
+    $tipo=strtolower($_POST['tipo1']);
+    $tipo_dos=strtolower($_POST['tipo2']);
 
-$sql = "UPDATE pokemons SET nombre = '$nombre', altura = '$altura', peso = '$peso', habilidad = '$habilidad', tipo = '$tipo', tipo_dos = '$tipo_dos', imagen = '$imagen', descripcion = '$descripcion' WHERE id ='$id';";
+    $imgFile = $_FILES['imagen']['name'];
+    $tmp_dir = $_FILES['imagen']['tmp_name'];
 
-$result = $con->query($sql);
+    $upload_dir = 'recursos/img/pokemons/';
 
-header("location:pagina-logueado.php");
+    move_uploaded_file($tmp_dir,$upload_dir.$imgFile);
 
-exit();
+    $id = isset( $_GET["id"])?$_GET["id"] : "";
 
-$con->close();
+    include_once("conexion.php");
 
+    $sql2 = "SELECT * FROM pokemons WHERE id_manual = '$id'";
 
+    $result2 = $con->query($sql2);
+
+    while ($fila = $result2->fetch_assoc()){
+
+        if($tipo_dos == ""){
+            $sql = "UPDATE pokemons
+            SET altura = '$altura', 
+            peso = '$peso', 
+            habilidad = '$habilidad', 
+            tipo = 'recursos/img/pokemons/tipo/$tipo.png', 
+            descripcion = '$descrip', 
+            imagen = 'recursos/img/pokemons/$imgFile'
+            WHERE id = $id";
+
+            $result = $con->query($sql);
+        }
+        else{
+            $sql = "UPDATE pokemons
+            SET altura = '$altura', 
+            peso = '$peso', 
+            habilidad = '$habilidad', 
+            tipo = 'recursos/img/pokemons/tipo/$tipo.png', 
+            tipo_dos = 'recursos/img/pokemons/tipo/$tipo_dos.png', 
+            descripcion = '$descrip', 
+            imagen = 'recursos/img/pokemons/$imgFile'
+            WHERE id = $id";
+
+            $result = $con->query($sql);
+        }
+
+        /*header("location:pagina-logueado.php");
+        exit();*/
+
+        // $con->close();
+
+    }
+    header("location:pagina-logueado.php");
+    exit();
+    $con->close();
+
+}
 
 ?>
