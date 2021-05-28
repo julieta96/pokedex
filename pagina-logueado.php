785808b4adb8602f5b108d7930b48175894f37f4
Tabatha <49240387+Tabathapm@users.noreply.github.com>
2021-05-28 20:35:10 -0300

    Algo de responsive

--- pagina-logueado.php
+++ pagina-logueado.php
@@ -1,63 +1,76 @@
 <?php
 session_start();
-if( !isset($_SESSION["usuario"]) ){
-      header("location:index.php");
-      exit();
-  	}
+
+if(!isset($_SESSION['usuario'])){
+    header("location:index.php");
+    exit();
+}
+
 include('header-con-sesion.php');
 include('conexion.php');
 include('busqueda-pokemon.php');
+
+
 ?>
 
-	<section class="d-flex flex-col mb-3 col">
+    <!--------------------- BUSQUEDA -------------------------------->
 
-		<form class="form-group d-flex flex-col col" method="get" action="pagina-logueado.php">
-			<input type="text"  class="form-control" name="buscar" placeholder="Ingrese nombre, tipo o numero de pokemon"><button type="submit" class="btn btn-primary" name="boton">Quien es este pokemon?</button>
-		</form>
-	 </section>
-   <table class="table container">
-  <thead>
-    <tr>
-      <th scope="col">Nro</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Altura</th>
-      <th scope="col">Peso</th>
-      <th scope="col">Habilidad</th>
-      <th scope="col">Tipo</th>
-      <th scope="col">Descripcion</th>
-      <th scope="col">Imagen</th>
-      <th scope="col">Acciones</th>
-    </tr>
-  </thead>
-  <tbody>
-       <?php
-      if(isset($_GET['boton'])){
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 py-3">
+                <form class="d-flex col" action="pagina-logueado.php" method="get">
+                    <input type="text"  class="form-control" name="buscar" placeholder="Ingrese nombre, tipo o numero de pokemon">
+                    <button type="submit" class="btn btn-primary" name="boton">Quien es este pokemon?</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
-        include('obtener-pokemon.php');
+    <!--------------------- TABLA -------------------------------->
+    <table class="table container">
+        <thead class="text-center">
+        <tr>
+            <th scope="col">Nro</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Altura</th>
+            <th scope="col" class="pe-5 ps-5">Peso</th>
+            <th scope="col">Habilidad</th>
+            <th scope="col">Tipo</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Imagen</th>
+            <th scope="col">Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if(isset($_GET['boton'])){
 
-      }else{
+            include('obtener-pokemon.php');
 
-       include('busqueda-pokemon.php');
-       include('tabla-pokemon.php');
+        }else{
 
-      }
-                  
-    ?>
+            include('busqueda-pokemon.php');
+            include('tabla-pokemon.php');
 
-    
-    <tr>
-    </tr>
-  </tbody>
-</table>
+        }
 
+        ?>
 
+        <tr>
+        </tr>
+        </tbody>
+    </table>
 
-<div class="container mt-3"><h3><a class="btn btn-success w-100" href="subirForm.php">SUBIR UN POKEMON</a></h3></div>
+    <div class="container pt-3">
+        <h3>
+            <a class="btn btn-primary w-100" href="subir-pokemon.php">CARGAR UN POKEMON</a>
+        </h3>
+    </div>
 
-	<script type="text/javascript" src="recursos/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="recursos/js/bootstrap.min.js"></script>
 
-</body>
-</html>
+    </body>
+    </html>
 
 <?php
 $con->close();
